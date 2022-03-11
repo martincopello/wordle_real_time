af3a856af4d006c6ab42fa036e65d3f6d26e0399
Martín Copello <mcopello18@gmail.com>
2022-03-10 21:19:45 -0600

    Add files via upload

--- client.py
+++ client.py
@@ -1,21 +1,41 @@
 import socket
 import sys
-
+import time
 HOST, PORT = "localhost", 9999
-data = "i am martin, hi" #Create string we want to capitalize
+#Create string we want to capitalize
 
 # Create a socket (SOCK_STREAM means a TCP socket)
+message = str()
+
+#Can implement the timed run with a while variable is False do nothing. but then have a timer to set it to true. set the variable to false at the end of a wordle
 while True:
+    global message
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: #creating the socket
-        solutions = []  # Connect to server and send data
+        #solutions = []  # Connect to server and send data
         sock.connect((HOST, PORT)) #we tell socket to connect to server
-        sock.sendall(bytes(data + "\n", "utf-8")) #send string that I created. Need to convert string to bytes before sending to server
-
-        # Receive data from the server and shut down
-        received = str(sock.recv(1024), "utf-8") #here we expect response from server
-
+        if message == "":
+            message = "get_puzzle"
+            sock.sendall(message.encode("utf-8")) #send string that I created. Need to convert string to bytes before sending to server
+            print("Sent: {}".format(message))
+            received = str(sock.recv(1024), "utf-8")
+            print("Received: {}".format(received))
+        elif received == "Wordle started. Go ahead and send me a guess.":
+            message = "adieu"
+            sock.sendall(message.encode("utf-8"))
+            print("Sent: {}".format(message))
+            received = str(sock.recv(1024), "utf-8")
+            print("Received: {}".format(received))
+        elif received == "You've already faield to solve the current Wordle!":
+            message = str()
+        elif received == "it's OVER!":
+            message = str()
+        else:
+            #this is currently sending "crony" over and over. this is where we would incorporate our solver and its guesses
+            message="crony"
+            sock.sendall(message.encode("utf-8"))
+            print("Sent: {}".format(message))
+            received = str(sock.recv(1024), "utf-8")
+            print("Received: {}".format(received))
         #if we receive a word from the server here we can feed it into our model and send back a word guess to the server
-        print("Sent:     {}".format(data))
-        print("Received: {}".format(received))
         sock.close()
-        time.sleep(5)
+        time.sleep(1)
--- server.py
+++ server.py
@@ -1,4 +1,51 @@
 import socketserver
+import random
+import time
+
+def create_list_of_words(path):
+    words = []
+    with open(path, 'r') as file:
+        for line in file:
+            words.append(line.rstrip())
+    file.close()
+    return(words)
+
+list_of_words = create_list_of_words('Project/FiveLetterWords.txt')
+
+def pick_word(words_list):
+    return(random.choice(words_list))
+
+def split_word(word):
+    return ({letter: index for index, letter in enumerate(word)})
+
+def set_timer():
+    t = random.randrange(5, 30, 1)
+    time.sleep(t)
+    reset_wordle()
+
+def reset_wordle():
+    global curr_word
+    global curr_solution
+    global curr_puzzle_solved
+    global wordle_ended
+    global number_of_guesses
+    global solve_count
+    global curr_guess
+    curr_word = pick_word(list_of_words)
+    curr_solution = split_word(curr_word)
+    curr_puzzle_solved = False
+    wordle_ended = False
+    number_of_guesses = 0
+    curr_guess = {}
+
+curr_word = pick_word(list_of_words)
+curr_solution = split_word(curr_word)
+curr_puzzle_solved = False
+wordle_ended = False
+number_of_guesses = 0
+solve_count = 0
+curr_guess = {}
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
@@ -10,12 +57,64 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     """
 
     def handle(self):
+        global curr_word
+        global curr_solution
+        global curr_puzzle_solved
+        global wordle_ended
+        global number_of_guesses
+        global solve_count
+        global curr_guess
+
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip() #receive string
+
+        #If the client requests a new wordle and they have not already solved it, start it up
+        if self.data.decode("utf-8") == "get_puzzle":
+            message = "Wordle started. Go ahead and send me a guess.".encode("utf-8")
+            self.request.sendall(message)
+        #If the client has already solved the current puzzle, deny them
+        elif curr_puzzle_solved == True:
+            message = "You've already solved the current Wordle!".encode("utf-8")
+            self.request.sendall(message)
+        #If the client already used up all five of its guesses, deny them
+        elif wordle_ended == True:
+            message = "You've already failed to solve the current Wordle!".encode("utf-8")
+            self.request.sendall(message)
+        #If the client's guess is invalid, deny them
+        elif len(self.data.decode("utf-8")) != 5 or not isinstance(self.data.decode("utf-8"), str):
+            message = "Your guess is not a 5-letter word, therefore invalid.".encode("utf-8")
+            self.request.sendall(message)
+        #If they guess the word, send a congratulations message!
+        elif self.data.decode("utf-8") == curr_word:
+            message = "Congratulations! You've solved the Wordle".encode("utf-8")
+            self.request.sendall(message)
+            solve_count +=1
+            reset_wordle()
+        #If the client reaches 5 guesses and they have not solved it then reset the wordle
+    elif number_of_guesses == 5:
+            message = "it's OVER!".encode("utf-8")
+            self.request.sendall(message)
+            wordle_ended == True
+            time.sleep(15)
+            reset_wordle()
+        #Otherwise increase the number of guesses and provide feedback to the client
+        else:
+            number_of_guesses += 1
+            curr_guess = split_word(self.data.decode("utf-8"))
+            message = {}
+            for letter in curr_guess.keys():
+                if letter in curr_solution.keys() and curr_guess[letter] == curr_solution[letter]:
+                    print(letter)
+                    message[letter]= "correct"
+                elif letter in curr_solution.keys():
+                    print(letter)
+                    message[letter]= "present"
+                else:
+                    print(letter)
+                    message[letter]= "absent"
+            message = str(str(message) + str(curr_word)).encode("utf-8")
+            self.request.sendall(message)
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper()) #capitalize string and send string to client
 
 if __name__ == "__main__":
     #HOST, PORT = "localhost", 9999
