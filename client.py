c8c416fc4da76a3d235306fe88f353e1fa7c27ef
Martín Copello <mcopello18@gmail.com>
2022-03-10 18:27:14 -0600

    Add files via upload

--- client.py
+++ client.py
@@ -2,16 +2,18 @@ import socket
 import sys
 
 HOST, PORT = "localhost", 9999
-data = "i am marty, hi" #Create string we want to capitalize
+data = "i am martin, hi" #Create string we want to capitalize
 
 # Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: #creating the socket
-    # Connect to server and send data
-    sock.connect((HOST, PORT)) #we tell socket to connect to server
-    sock.sendall(bytes(data + "\n", "utf-8")) #send string that I created. Need to convert string to bytes before sending to server
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: #creating the socket
+        solutions = []  # Connect to server and send data
+        sock.connect((HOST, PORT)) #we tell socket to connect to server
+        sock.sendall(bytes(data + "\n", "utf-8")) #send string that I created. Need to convert string to bytes before sending to server
 
-    # Receive data from the server and shut down
-    received = str(sock.recv(1024), "utf-8") #here we expect response from server
-
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+        # Receive data from the server and shut down
+        received = str(sock.recv(1024), "utf-8") #here we expect response from server
+        print("Sent:     {}".format(data))
+        print("Received: {}".format(received))
+        sock.close()
+        time.sleep(5)
